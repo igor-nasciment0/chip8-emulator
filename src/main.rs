63af7272e90d126b31eb6caca58ec58735bfd549
igor-nasciment0 <nascimentoigor1311@gmail.com>
2026-01-03 16:17:50 -0300

    init

---
+++ src/main.rs
@@ -0,0 +1,55 @@
+mod emulator;
+mod canvas;
+use std::process::exit;
+
+use sdl2::pixels::Color;
+use sdl2::event::Event;
+use sdl2::keyboard::Keycode;
+use std::time::Duration;
+
+use crate::emulator::Emulator;
+
+fn main() {
+    let mut emulator = Emulator::new();
+
+    if let Err(rom) = emulator.load_rom("./alago") {
+        println!("{rom}");
+        exit(1);
+    }
+
+    let sdl_context = sdl2::init().unwrap();
+    let video_subsystem = sdl_context.video().unwrap();
+
+    let window = video_subsystem
+        .window("Chip-8 Emulator", 640, 320)
+        .position_centered()
+        .build()
+        .unwrap();
+
+    let mut canvas = window.into_canvas().present_vsync().build().unwrap();
+
+    canvas.set_draw_color(Color::RGB(0, 0, 0));
+    canvas.clear();
+    canvas.present();
+
+    let mut event_pump = sdl_context.event_pump().unwrap();
+
+    'running: loop {
+        for event in event_pump.poll_iter() {
+            match event {
+                Event::Quit { .. }
+                | Event::KeyDown {
+                    keycode: Some(Keycode::Escape),
+                    ..
+                } => {
+                    break 'running;
+                }
+                _ => {}
+            }
+        }
+
+        // FDC cycle
+
+        std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
+    }
+}
